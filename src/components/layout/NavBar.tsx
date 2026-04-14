b870ccf04b52b17f57cb005a6b2657694b041d31
Claude Dev <dev@livenni.com.br>
2026-04-14 17:55:54 -0300

    update: increase mobile logo size further

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -115,7 +115,7 @@ export default function Navbar() {
                 <img
                   src="/images/logo-preto.png"
                   alt="Livenni Logo"
-                  className="w-16 h-16 object-contain"
+                  className="w-32 h-32 object-contain"
                 />
                 <button
                   onClick={() => setMenuOpen(false)}
